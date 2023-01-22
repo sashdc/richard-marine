d604a264289232007ac6a501f66cdcd1d6295909
saharsh <saharshdc@gmail.com>
2023-01-22 14:05:07 -0500

    heroku deployment

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,13 +8,13 @@ export default function About() {
  data-ride="carousel" data-interval="2500">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img className="d-block w-50 rounded" src="./rms-website/carousel/carousel1.jpg" alt="First slide"/>
+      <img className="d-block w-50 rounded" src="./carousel/carousel1.jpg" alt="First slide"/>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-50 rounded" src="./rms-website/carousel/carousel2.jpg" alt="Second slide"/>
+      <img className="d-block w-50 rounded" src="./carousel/carousel2.jpg" alt="Second slide"/>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-50 rounded" src="./rms-website/carousel/carousel3.jpg" alt="Third slide"/>
+      <img className="d-block w-50 rounded" src="./carousel/carousel3.jpg" alt="Third slide"/>
     </div>
   </div>
   <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -30,19 +30,19 @@ export default function About() {
       <div className='about d-flex w-75 mt-5 flex-column'>
       <h1 >ABOUT RMSM</h1>
       <div className='d-flex m-2 about-segment'>
-        <img src="./rms-website/images/britannia-foundry.png" className = "rounded about-image" alt="first RMSM foundry" height="250px" />
+        <img src="./images/britannia-foundry.png" className = "rounded about-image" alt="first RMSM foundry" height="250px" />
         <p className='about-text m-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
       
       </div>
 
       <div className='d-flex m-2 about-segment'>
         <p className='about-text m-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-        <img src="./rms-website/images/bollard-skyline.jpg" className = "rounded about-image" alt="RMSM bollard against dubai skyline" height="300px" />
+        <img src="./images/bollard-skyline.jpg" className = "rounded about-image" alt="RMSM bollard against dubai skyline" height="300px" />
 
       </div>
 
       <div className='d-flex m-2 about-segment'>
-        <img src="./rms-website/images/bollard-name.jpg" className = "rounded about-image" alt="first RMSM foundry" height="250px" />
+        <img src="./images/bollard-name.jpg" className = "rounded about-image" alt="first RMSM foundry" height="250px" />
         <p className='about-text m-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
       
       </div>
