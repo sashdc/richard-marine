af77148564132b7daa55a2a3894515a73f3da926
Saharsh Cherian <saharshdc@gmail.com>
2023-02-23 16:35:52 -0500

    fixed routing on notfound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,14 @@
 import React from "react";
 import notfound from "../assets/images/404.jpg";
 import "../styles/notfound.css";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
     <div className="m-auto mt-5  animate__animated animate__fadeIn ">
       <div className="m-auto notfound ">
    
-        <a href="/">
+      <Link to="/" className="nav-link">
           <img
             src={notfound}
             className="rounded shadow"
@@ -15,7 +16,7 @@ export default function NotFound() {
             alt="404"
           />
          <button type="button" className="btn btn-warning mt-4"> Uh-oh! Looks like you're lost. Click here to go back home</button>
-        </a>
+        </Link>
       </div>
     </div>
   );
