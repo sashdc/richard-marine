9667f580e40110fe6abc6434650ca9975c77c86d
Saharsh Cherian <saharshdc@gmail.com>
2023-03-07 10:14:59 -0500

    changed logo animation and navbar on mobile

--- src/components/Header copy.js
+++ src/components/Header copy.js
@@ -25,7 +25,7 @@ export default function Header() {
         <div className="collapse navbar-collapse " id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-            <Link to="/about" className="nav-link" >
+            <Link to="/about" className="nav-link">
                 About <span className="sr-only">(current)</span>
               </Link>
             </li>
