01de61ea6e9d5782fd6a777446c759e5270db75f
Saharsh Cherian <saharshdc@gmail.com>
2023-03-06 20:28:42 -0500

    fixed carousel jpg extension and added caarousel alts

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,22 +10,22 @@ export default function Carousel() {
       <MDBCarouselItem
         className='w-100 d-block '
         itemId={1}
-        src="./richard-marine/carousel/carousel1.jpg"        alt='...'
+        src="./richard-marine/carousel/carousel1.jpg"        alt='close up of bollard with company mark'
       />
       <MDBCarouselItem
         className='w-100 d-block'
         itemId={2}
-        src="./richard-marine/carousel/carousel2.jpg"        alt='...'
+        src="./richard-marine/carousel/carousel2.jpg"        alt='a bollard close up with the dock extending into the background'
       />
       <MDBCarouselItem
         className='w-100 d-block'
         itemId={3}
-        src="./richard-marine/carousel/carousel3.jpg"        alt='...'
+        src="./richard-marine/carousel/carousel3.jpg"        alt='a nmber of yellow bollards on a factory floor'
       />
          <MDBCarouselItem
         className='w-100 d-block'
         itemId={3}
-        src="./richard-marine/carousel/carousel4.jpg"        alt='...'
+        src="./richard-marine/carousel/carousel4.jpg"        alt='a dock seen from teh water with bolalrds visible'
       />
     </MDBCarousel>
   );
