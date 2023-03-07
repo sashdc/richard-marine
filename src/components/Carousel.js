b127acafd6b0191407a535d20c9e66f8cac5282d
Saharsh Cherian <saharshdc@gmail.com>
2023-03-07 10:40:31 -0500

    second carousel image

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -13,19 +13,19 @@ export default function Carousel() {
         <MDBCarouselItem
         className="w-100 d-block "
         itemId={2}
-        src="./richard-marine/carousel/carousel2.jpg"
+        src="./richard-marine/carousel/carouseltwo.jpg"
         alt="a bollard close up with the dock extending into the background"      />
          <MDBCarouselItem
         className="w-100 d-block"
         itemId={3}
         src="./richard-marine/carousel/carousel3.jpg"
-        alt="a nmber of yellow bollards on a factory floor"
+        alt="a number of yellow bollards on a factory floor"
       />
       <MDBCarouselItem
         className="w-100 d-block"
         itemId={4}
         src="./richard-marine/carousel/carousel4.jpg"
-        alt="a dock seen from teh water with bolalrds visible"
+        alt="a dock seen from the water with bollards visible"
       />
     </MDBCarousel>
   );
