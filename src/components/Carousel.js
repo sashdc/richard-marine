8fa5dab17c08a00ffaad64cf41537d7a65ea0dc5
Saharsh Cherian <saharshdc@gmail.com>
2023-03-07 09:16:10 -0500

    second carousel image

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-} from 'mdb-react-ui-kit';
+import React from "react";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
 export default function Carousel() {
   return (
-    <MDBCarousel showControls fade className='carousel'>
+    <MDBCarousel showControls fade className="carousel">
       <MDBCarouselItem
-        className='w-100 d-block '
+        className="w-100 d-block "
         itemId={1}
-        src="./richard-marine/carousel/carousel1.jpg"        alt='close up of bollard with company mark'
+        src="./richard-marine/carousel/carousel1.jpg"
+        alt="close up of bollard with company mark"
       />
       <MDBCarouselItem
-        className='w-100 d-block'
+        className="w-100 d-block"
         itemId={2}
-        src="./richard-marine/carousel/carousel2.jpg"        alt='a bollard close up with the dock extending into the background'
+        src="./richard-marine/carousel/carousel2.jpg"
+        alt="a bollard close up with the dock extending into the background"
       />
       <MDBCarouselItem
-        className='w-100 d-block'
+        className="w-100 d-block"
         itemId={3}
-        src="./richard-marine/carousel/carousel3.jpg"        alt='a nmber of yellow bollards on a factory floor'
+        src="./richard-marine/carousel/carousel3.jpg"
+        alt="a nmber of yellow bollards on a factory floor"
       />
-         <MDBCarouselItem
-        className='w-100 d-block'
+      <MDBCarouselItem
+        className="w-100 d-block"
         itemId={3}
-        src="./richard-marine/carousel/carousel4.jpg"        alt='a dock seen from teh water with bolalrds visible'
+        src="./richard-marine/carousel/carousel4.jpg"
+        alt="a dock seen from teh water with bolalrds visible"
       />
     </MDBCarousel>
   );
